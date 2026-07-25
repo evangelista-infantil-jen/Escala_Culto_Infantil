573e9d2b1e63c6e06e92cc12d5df451beca257cf
evangelista-infantil-jen <evangelista.infantil.jen@gmail.com>
2026-07-25 16:08:06 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,26 +1,15 @@
-import { initializeApp } from
-"https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 
-import {
-getFirestore
-}
-from
-"https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
 const firebaseConfig = {
-
-apiKey:"COLOQUE_AQUI",
-
-authDomain:"COLOQUE_AQUI",
-
-projectId:"COLOQUE_AQUI",
-
-storageBucket:"COLOQUE_AQUI",
-
-messagingSenderId:"COLOQUE_AQUI",
-
-appId:"COLOQUE_AQUI"
-
+  apiKey: "AIzaSyBnhpeV6nenhdiaeWMjMR6F8xq0aA-yg3g",
+  authDomain: "escala-culto-infantil.firebaseapp.com",
+  projectId: "escala-culto-infantil",
+  storageBucket: "escala-culto-infantil.firebasestorage.app",
+  messagingSenderId: "451702979289",
+  appId: "1:451702979289:web:a43f34a184f0d20757dcc8",
+  measurementId: "G-F1VB6HBC9M"
 };
 
 const app = initializeApp(firebaseConfig);
